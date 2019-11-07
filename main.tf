954e6d98acd0d8137194e972359b1296db45655e
Noman Sadiq <nomi.cs786@gmail.com>
2019-11-07 18:54:43 +0400

    fix resouce group refer

--- main.tf
+++ main.tf
@@ -13,8 +13,8 @@ resource "azurerm_resource_group" "PaymentFacade" {
 
 resource "azurerm_network_security_group" "test" {
   name                = "acceptanceTestSecurityGroup1"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.PaymentFacade.name}"
 }
 
 resource "azurerm_network_ddos_protection_plan" "test" {
