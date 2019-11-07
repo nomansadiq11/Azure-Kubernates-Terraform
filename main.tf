e90b545db3f52c52d85e938aa938606f2031e917
Noman Sadiq <nomi.cs786@gmail.com>
2019-11-07 18:57:01 +0400

    updated network resouce group name

--- main.tf
+++ main.tf
@@ -25,9 +25,9 @@ resource "azurerm_network_ddos_protection_plan" "test" {
 
 resource "azurerm_virtual_network" "test" {
   name                = "PaymentSecVNet-Dev"
-  location            = "${azurerm_resource_group.test.location}"
-  resource_group_name = "${azurerm_resource_group.test.name}"
-  address_space       = ["10.0.0.0/24"]
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.PaymentFacade.name}"
+  address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 
   ddos_protection_plan {
