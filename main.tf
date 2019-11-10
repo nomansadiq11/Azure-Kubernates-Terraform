42f8384b08c709ec87877f8519358d39b3b61f3b
Noman Sadiq <nomi.cs786@gmail.com>
2019-11-10 11:27:34 +0400

    virtual network created and updated address space

--- main.tf
+++ main.tf
@@ -27,8 +27,8 @@ resource "azurerm_virtual_network" "test" {
   name                = "PaymentSecVNet-Dev"
   location            = "${var.location}"
   resource_group_name = "${azurerm_resource_group.PaymentFacade.name}"
-  address_space       = ["10.0.0.0/16"]
-  dns_servers         = ["10.0.0.4", "10.0.0.5"]
+  address_space       = ["10.140.0.0/16"]
+  dns_servers         = ["10.140.0.4", "10.140.0.5"]
 
   ddos_protection_plan {
     id     = "${azurerm_network_ddos_protection_plan.ddospplan1.id}"
@@ -37,17 +37,52 @@ resource "azurerm_virtual_network" "test" {
 
   subnet {
     name           = "subnet1"
-    address_prefix = "10.0.1.0/24"
+    address_prefix = "10.140.1.0/24"
   }
 
   subnet {
     name           = "subnet2"
-    address_prefix = "10.0.2.0/24"
+    address_prefix = "10.140.2.0/24"
   }
 
   subnet {
     name           = "subnet3"
-    address_prefix = "10.0.3.0/24"
+    address_prefix = "10.140.3.0/24"
+    security_group = "${azurerm_network_security_group.acceptanceTestSecurityGroup1.id}"
+  }
+
+  tags = {
+    environment = "${var.tag}"
+  }
+}
+
+# Virtual Network PaymentIngeVNET
+
+resource "azurerm_virtual_network" "test" {
+  name                = "PaymentIntegVNET-Dev"
+  location            = "${var.location}"
+  resource_group_name = "${azurerm_resource_group.PaymentFacade.name}"
+  address_space       = ["10.141.0.0/16"]
+  dns_servers         = ["10.141.0.4", "10.141.0.5"]
+
+  ddos_protection_plan {
+    id     = "${azurerm_network_ddos_protection_plan.ddospplan1.id}"
+    enable = true
+  }
+
+  subnet {
+    name           = "subnet1"
+    address_prefix = "10.141.1.0/24"
+  }
+
+  subnet {
+    name           = "subnet2"
+    address_prefix = "10.141.2.0/24"
+  }
+
+  subnet {
+    name           = "subnet3"
+    address_prefix = "10.141.3.0/24"
     security_group = "${azurerm_network_security_group.acceptanceTestSecurityGroup1.id}"
   }
 
