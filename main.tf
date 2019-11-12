bc2c14d72cb2b4fa15125cdc9569e13529fc83cd
Noman Sadiq <nomi.cs786@gmail.com>
2019-11-12 17:40:12 +0400

    updated properties

--- main.tf
+++ main.tf
@@ -186,19 +186,19 @@ resource "azurerm_cosmosdb_account" "db" {
   kind                = "GlobalDocumentDB"
 
   enable_automatic_failover = false
-  enable_Multiple_Write_Locations = false
+  enable_multiple_write_locations = false
 
 
   consistency_policy {
     consistency_level       = "Session"
-    max_interval_in_seconds = 10
-    max_staleness_prefix    = 200
+    max_interval_in_seconds = 5
+    max_staleness_prefix    = 100
   }
 
-  # geo_location {
-  #   location          = "${var.failover_location}"
-  #   failover_priority = 1
-  # }
+  geo_location {
+    location          = "${var.failover_location}"
+    failover_priority = 1
+  }
 
   # geo_location {
   #   prefix            = "paymentfacadedev-db-${random_integer.ri.result}-customid"
