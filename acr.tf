893c39406178c862fdf287e117eada0e0d5d9106
Noman Sadiq <nomi.cs786@gmail.com>
2020-03-12 19:08:16 +0400

    remove aks

--- acr.tf
+++ acr.tf
@@ -1,25 +1,25 @@
 
-resource "azurerm_container_registry" "acr" {
-  name                = "acspaymenttest"
-  resource_group_name = "${var.resouce_group_name}"
-  location            = "${var.location}"
-  sku                 = "basic"
-  admin_enabled       = false
-}
+# resource "azurerm_container_registry" "acr" {
+#   name                = "acspaymenttest"
+#   resource_group_name = "${var.resouce_group_name}"
+#   location            = "${var.location}"
+#   sku                 = "basic"
+#   admin_enabled       = false
+# }
 
 
 
-resource "azurerm_role_definition" "acs-assign-acr-role" {
-  name        = "assign-role-acr"
-  scope       = "${azurerm_container_registry.acr.id}"
-  description = "This is a custom role created via Terraform"
+# resource "azurerm_role_definition" "acs-assign-acr-role" {
+#   name        = "assign-role-acr"
+#   scope       = "${azurerm_container_registry.acr.id}"
+#   description = "This is a custom role created via Terraform"
 
-  permissions {
-    actions     = ["*"]
-    not_actions = []
-  }
+#   permissions {
+#     actions     = ["*"]
+#     not_actions = []
+#   }
 
-  assignable_scopes = [
-    "${azurerm_container_registry.acr.id}", 
-  ]
-}
+#   assignable_scopes = [
+#     "${azurerm_container_registry.acr.id}", 
+#   ]
+# }
