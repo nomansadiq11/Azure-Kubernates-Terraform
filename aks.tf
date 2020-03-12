3124b4a68e3764718aa8dad28185b3b2439f3558
Noman Sadiq <nomi.cs786@gmail.com>
2020-03-12 18:33:17 +0400

    advance networking added for cluster

--- aks.tf
+++ aks.tf
@@ -6,17 +6,28 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
   location            = "${var.location}"
   dns_prefix          = "akscluster"
 
-  default_node_pool {
-    name            = "default"
-    node_count      = 3
-    vm_size         = "Standard_D2s_v3"
-    
-  }
+  agent_pool_profile {
+        name            = "nodepool01"
+        count           = "3"
+        vm_size         = "Standard_DS2_v2"
+        os_type         = "Linux"
+        os_disk_size_gb = 30
+
+        # Required for advanced networking
+        vnet_subnet_id = "${azurerm_subnet.cluster.id}"
+    }
 
   service_principal {
     client_id     = "clientid"
     client_secret = "secretkey"
   }
+
+  addon_profile {
+        oms_agent {
+            enabled = true
+            log_analytics_workspace_id = "${azurerm_log_analytics_workspace.aks.id}"
+        }
+    }
 
 
 
--- vnet.tf
+++ vnet.tf
@@ -12,3 +12,30 @@ resource "azurerm_virtual_network" "aks-Dev" {
 
 
 }
+
+
+resource "azurerm_subnet" "cluster" {
+  name                  = "cluster-01"
+  resource_group_name   = "${azurerm_resource_group.aks.name}"
+  address_prefix        = "10.140.0.0/24"
+  virtual_network_name  = "${azurerm_virtual_network.aks-Dev.name}"
+  route_table_id        = "${azurerm_route_table.cluster-01.id}"
+}
+
+
+resource "azurerm_route_table" "cluster-01" {
+  name                = "${azurerm_resource_group.akscluster.name}-routetable"
+  location            = "${azurerm_resource_group.akscluster.location}"
+  resource_group_name = "${azurerm_resource_group.akscluster.name}"
+
+  route {
+    name                   = "cluster-01"
+    address_prefix         = "10.100.0.0/14"
+    next_hop_type          = "VirtualAppliance"
+    next_hop_in_ip_address = "10.10.1.1"
+  }
+}
+resource "azurerm_subnet_route_table_association" "cluster-01" {
+  subnet_id      = "${azurerm_subnet.cluster.id}"
+  route_table_id = "${azurerm_route_table.cluster-01.id}"
+}
